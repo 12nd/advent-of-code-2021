93984a409d8ebe5ea42723d6764f31731cb02253
James Ho <ho.james142@gmail.com>
2021-12-04 22:23:51 +1100

    Day 02 pt 2

--- day02/day02.c
+++ day02/day02.c
@@ -1,33 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    FILE *input;
+struct COMMAND {
+    char dir;
+    int dist;
+};
+
+struct COMMAND parseLine(char *line) {
+    struct COMMAND out;
+    out.dir = line[0];
+    if (out.dir == 'f') {
+        sscanf(line+8, "%d", &out.dist);
+    } else if (out.dir == 'd') {
+        sscanf(line+5, "%d", &out.dist);
+    } else if (out.dir == 'u') {
+        sscanf(line+3, "%d", &out.dist);
+    } else {
+        printf("unreachable\n");
+        exit(1);
+    }
+    return out;
+}
+
+int main(void) {
+    FILE *inFile;
     int posX = 0;
     int posY = 0;
-    char line[10];
-    int dist;
+    int aim = 0;
+    int posH = 0;
+    int posD = 0;
+    char line[20];
 
-    input = fopen("input.txt", "r");
+    inFile = fopen("input.txt", "r");
 
-    while (fgets(line, 10, input) != NULL) {
-        if (line[0] == 'f') {
-            sscanf(line+8, "%d", &dist);
-            posX += dist;
-        }
-        else if (line[0] == 'd') {
-            sscanf(line+5, "%d", &dist);
-            posY += dist;
-        }
-        else if (line[0] == 'u') {
-            sscanf(line+3, "%d", &dist);
-            posY -= dist;
+    while (fgets(line, 20, inFile) != NULL) {
+        struct COMMAND cmd = parseLine(line);
+        if (cmd.dir == 'f') {
+            posX += cmd.dist;
+            posH += cmd.dist;
+            posD += aim * cmd.dist;
+        } else if (cmd.dir == 'd') {
+            posY += cmd.dist;
+            aim += cmd.dist;
+        } else if (cmd.dir == 'u') {
+            posY -= cmd.dist;
+            aim -= cmd.dist;
         }
     }
-    fclose(input);
+    fclose(inFile);
 
-    printf("%d\n", posX);
-    printf("%d\n", posY);
     printf("%d\n", posX * posY);
+    printf("%d\n", posH * posD);
 
     return 0;
 }
