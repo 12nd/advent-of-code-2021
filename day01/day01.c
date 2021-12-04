28ebddd6386e97ec1ac123b491b112fcdda0c200
James Ho <ho.james142@gmail.com>
2021-12-04 12:55:06 +1100

    Day 01 both parts together

--- day01/day01.c
+++ day01/day01.c
@@ -4,21 +4,29 @@ int main(void) {
   int window[3];
   int indx = 0;
   int cur;
-  int tot = 0;
+  int tot1 = 0;
+  int tot2 = 0;
 
   for (int i=0; i<3; ++i) {
     scanf("%d", &cur);
     window[i] = cur;
+    if (i > 0) {
+      if (cur > window[i-1])
+        ++tot1;
+    }
   }
 
   while (scanf("%d", &cur) == 1) {
+    if (cur > window[(indx+2)%3])
+      ++tot1;
     if (cur - window[indx] > 0)
-      ++tot;
+      ++tot2;
     window[indx] = cur;
     if (++indx == 3)
       indx = 0;
   }
-  printf("%d\n", tot);
+  printf("%d\n", tot1);
+  printf("%d\n", tot2);
 
   return 0;
 }
