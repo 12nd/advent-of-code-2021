8d4214fc49dfa6ecd01f8ecef105f67bcf2a5d77
James Ho <ho.james142@gmail.com>
2021-12-07 13:19:31 +1100

    Day 05 pt 2

--- day05/day05.c
+++ day05/day05.c
@@ -2,36 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        printf("Too few arguments\n");
+        printf("for part 1: %s 1\n", argv[0]);
+        printf("for part 2: %s 2\n", argv[0]);
+        return 1;
+    }
+
     FILE *inpFile = fopen("input.txt", "r");
-    int area[1000][1000] = {0};
+    int area[1100][1100] = {0};
     int dCount = 0;
 
     /* read input  */
-    int a,b,c,d;
-    while (fscanf(inpFile,"%d,%d -> %d,%d", &a, &b, &c, &d) != EOF) {
-        if (a == c) {
-            int x1 = b, x2 = d;
-            if (b > d)
-                x1 = d, x2 = b;
-            for (int i=x1; i<=x2; ++i) {
-                if (++area[i][a] == 2)
+    int x1,y1,x2,y2;
+    while (fscanf(inpFile,"%d,%d -> %d,%d", &x1, &y1, &x2, &y2) != EOF) {
+        if (x1 == x2) {
+            int xInc = y1 > y2 ? -1:1;
+            for (int i=y1; i != y2+xInc; i+=xInc) {
+                if (++area[x1][i] == 2)
                     ++dCount;
             }
-        } else if (b == d) {
-            int y1 = a, y2 = c;
-            if (a > c)
-                y1 = c, y2 = a;
-            for (int i=y1; i<=y2; ++i) {
-                if (++area[b][i] == 2)
+        } else if (y1 == y2) {
+            int yInc = x1 > x2 ? -1:1;
+            for (int i=x1; i != x2+yInc; i+=yInc) {
+                if (++area[i][y1] == 2)
                     ++dCount;
             }
-        } else {
-            continue;
+        /* diagonals */
+        } else if (atoi(argv[1]) == 2){
+            int xInc = x1 > x2 ? -1:1;
+            int yInc = y1 > y2 ? -1:1;
+            int y = y1;
+            for (int i=x1; i != x2+xInc; i+=xInc) {
+                if (++area[i][y] == 2) {
+                    ++dCount;
+                }
+                y += yInc;
+            }
         }
-
     }
-    printf("%d\n",dCount);
+    fclose(inpFile);
 
+    printf("%d\n",dCount);
+    return 0;
 }
