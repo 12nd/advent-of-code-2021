a90c815007acbc1c137069b0ca317b684d6a0bb2
James Ho <ho.james142@gmail.com>
2021-12-08 15:23:57 +1100

    Day 07 pt 2

--- day07/day07.c
+++ day07/day07.c
@@ -6,10 +6,43 @@ int compare (const void *a, const void *b)
    return ( *(int*)a - *(int*)b );
 }
 
+int part1(int *crabs, unsigned long n_crabs)
+{
+    qsort(crabs, n_crabs, sizeof(int), compare);
+    int med = crabs[n_crabs >> 1];
+    int tot_fuel = 0;
+    for (unsigned long i=0; i<n_crabs; ++i) {
+        tot_fuel += abs(crabs[i] - med);
+    }
+    return tot_fuel;
+}
+
+int fuel_cost(int n)
+{
+    return (n * (n+1)) >> 1;
+}
+
+int part2(int *crabs, unsigned long n_crabs)
+{
+    int sum_dist = 0;
+    for (unsigned long i=0; i<n_crabs; ++i) {
+        sum_dist += crabs[i];
+    }
+    int avg_f = sum_dist / (int)n_crabs;
+    int tot_fuel_f = 0;
+    int tot_fuel_c = 0;
+    for (unsigned long i=0; i<n_crabs; ++i) {
+        tot_fuel_f += fuel_cost(abs(crabs[i] - avg_f));
+        tot_fuel_c += fuel_cost(abs(crabs[i] - avg_f+1));
+    }
+    /* return min fuel cost */
+    return tot_fuel_f < tot_fuel_c ? tot_fuel_f:tot_fuel_c;
+}
+
 int main(int argc, char **argv)
 {
     if (argc < 2) {
-        printf("Usage: %s <input file>", argv[0]);
+        printf("Usage: %s <input file>\n", argv[0]);
         exit(1);
     }
 
@@ -23,15 +56,8 @@ int main(int argc, char **argv)
     }
     fclose(inp_file);
 
-    qsort(crabs, n_crabs, sizeof(int), compare);
-
-    int med = crabs[n_crabs >> 1];
-    int tot_fuel = 0;
-    for (unsigned long i=0; i<n_crabs; ++i) {
-        tot_fuel += abs(crabs[i] - med);
-    }
-
-    printf("%d", tot_fuel);
+    printf("%d\n", part1(crabs, n_crabs));
+    printf("%d\n", part2(crabs, n_crabs));
 
     return 0;
 }
