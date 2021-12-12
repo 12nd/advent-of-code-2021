f07c78ef52d0a950607920a9c16d0c665ed9188c
James Ho <ho.james142@gmail.com>
2021-12-12 18:51:10 +1100

    Day 10 pt 2
    Had some issues which required using larger data types like unsigned
    long long which in turn caused more issues in the sorting comparison
    function, which needed to be changed to one which does not use
    subtraction to compare.

--- day10/day10.c
+++ day10/day10.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #define BUFFER_CAP 1024
 
@@ -20,14 +21,63 @@ void push(struct STACK *stack, char x)
     stack->items[++stack->top] = x;
 }
 
-int pop(struct STACK *stack)
+char pop(struct STACK *stack)
 {
     return stack->items[stack->top--];
 }
 
+int is_empty(struct STACK *stack)
+{
+    return stack->top == -1;
+}
 
+/* compare function without subtraction to work with unsigned long long */
+int compare (const void *a, const void *b)
+{
+    const unsigned long ai = *( const unsigned long* )a;
+    const unsigned long bi = *( const unsigned long* )b;
+    if(ai < bi) {
+        return -1;
+    } else if(ai > bi) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
-char find_corrupt(char *line)
+int get_score(char bracket, int part)
+{
+    if (part == 1) {
+        switch(bracket) {
+            case ')':
+                return 3;
+            case ']':
+                return 57;
+            case '}':
+                return 1197;
+            case '>':
+                return 25137;
+        }
+    } else if (part == 2) {
+        switch(bracket) {
+            case '(':
+                return 1;
+            case '[':
+                return 2;
+            case '{':
+                return 3;
+            case '<':
+                return 4;
+        }
+    }
+    return 0;
+}
+
+unsigned long long pt2[BUFFER_CAP] = {0};
+int pt2_len = 0;
+
+/* finds corrupt lines (also fixes unfinished lines for part 2) */
+int find_corrupt(char *line)
 {
     /* initialise stack (probably should have a function to do this) */
     struct STACK *stack = (struct STACK*)malloc(sizeof(struct STACK));
@@ -41,25 +91,15 @@ char find_corrupt(char *line)
         /* if close bracket is not equal to the corresponding close from stack */
         else if (pop(stack) != *(strchr(b_c_pair, bracket) + 1)) {
             free(stack);
-            return bracket;
+            return get_score(bracket, 1);
         }
     }
+    unsigned long long score = 0;
+    while (!is_empty(stack)) {
+        score = score * 5 + (unsigned long long)get_score(pop(stack), 2);
+    }
+    pt2[pt2_len++] = score;
     free(stack);
-    return 0;
-}
-
-int get_score(char bracket)
-{
-    switch(bracket) {
-            case ')':
-                return 3;
-            case ']':
-                return 57;
-            case '}':
-                return 1197;
-            case '>':
-                return 25137;
-        }
     return 0;
 }
 
@@ -73,15 +113,18 @@ int main(int argc, char **argv)
     FILE *inp_file = fopen(argv[1], "r");
     char line[BUFFER_CAP] = {0};
 
-    int score = 0;
+    int pt1 = 0;
 
     while (fgets(line, BUFFER_CAP, inp_file) != NULL) {
         line[strcspn(line, "\n")] = 0;
 
-        score += get_score(find_corrupt(line));
+        pt1 += find_corrupt(line);
     }
+
+    qsort(pt2, (unsigned long)pt2_len, sizeof(unsigned long long), compare);
 
     fclose(inp_file);
 
-    printf("%d", score);
+    printf("%d\n", pt1);
+    printf("%llu\n", pt2[pt2_len >> 1]);
 }
