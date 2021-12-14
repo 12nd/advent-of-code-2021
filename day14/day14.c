ee34128e0abd7ff3c1140305fe6eec4b2231378c
James Ho <ho.james142@gmail.com>
2021-12-15 01:24:02 +1100

    Day 14 pt 2
    Simple refactor of part 1 to use int64_t (no more unsigned long longs).

--- day14/day14.c
+++ day14/day14.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #define BUFFER_CAP 1000
 
-
-int pairs[26][26] = {0};
+int64_t pairs[26][26] = {0};
 int rules[26][26] = {0};
-int ele_count[26] = {0};
+int64_t ele_count[26] = {0};
 
 int compare (const void *a, const void *b)
 {
-    return (*(int*)a - *(int*)b);
+    const int64_t ai = *(const int64_t*)a;
+    const int64_t bi = *(const int64_t*)b;
+    if (ai < bi)
+        return -1;
+    else if (ai > bi)
+        return 1;
+    else
+        return 0;
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
-        printf("Usage: %s <input file>\n", argv[0]);
+    if (argc < 3) {
+        printf("Usage: %s <input file> <steps>\n", argv[0]);
         exit(1);
     }
 
@@ -38,14 +45,13 @@ int main(int argc, char **argv)
     memset(rules, -1, sizeof(int) * 26 * 26);
     fseek(inp_file, (int)ftell(inp_file) + 1, SEEK_SET);
     char a, b, c;
-    while (fscanf(inp_file, "%c%c -> %c\n", &a, &b, &c) != EOF) {
+    while (fscanf(inp_file, "%c%c -> %c\n", &a, &b, &c) != EOF)
         rules[a - 'A'][b - 'A'] = c - 'A';
-    }
     fclose(inp_file);
 
-    int pairs_copy[26][26] = {0};
-    for (int _=0; _<10; ++_) {
-        memcpy(pairs_copy, pairs, sizeof(int) * 26 * 26);
+    int64_t pairs_copy[26][26] = {0};
+    for (int _=0; _<atoi(argv[2]); ++_) {
+        memcpy(pairs_copy, pairs, sizeof(int64_t) * 26 * 26);
         for (int l=0; l<26; ++l) {
             for (int r=0; r<26; ++r) {
                 if (pairs_copy[l][r] > 0 && rules[l][r] != -1) {
@@ -58,11 +64,13 @@ int main(int argc, char **argv)
             }
         }
     }
-    qsort(ele_count, 26, sizeof(int), compare);
+
+    qsort(ele_count, 26, sizeof(int64_t), compare);
     for (int i=0; i<26; ++i) {
         if (ele_count[i] > 0) {
-            printf("%d\n", ele_count[25] - ele_count[i]);
+            printf("%ld\n", ele_count[25] - ele_count[i]);
             break;
         }
     }
+    return 0;
 }
