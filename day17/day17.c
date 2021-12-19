9767c71e7b54115a8ca264291211c358777201d7
James Ho <ho.james142@gmail.com>
2021-12-19 22:24:52 +1100

    Day 17 pt 2
    Relatively simple day, implemented part 2 with a recursive function
    which runs the probe trajectory and a 'brute force' for loop for all the
    possible starting velocities.

--- day17/day17.c
+++ day17/day17.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int x1,x2,y1,y2;
+
+/* px, py = probe position at each step */
+/* vx, vy = velocity */
+int launch(int px, int py, int vx, int vy)
+{
+    px += vx;
+    py += vy;
+    /* overshot */
+    if (px > x2 || py < y1)
+        return 0;
+    /* hit */
+    if (px >= x1 && py <= y2)
+        return 1;
+    /* have not overshot or hit */
+    return launch(px, py, vx - (vx > 0), vy - 1);
+}
 
 int main(int argc, char **argv)
 {
@@ -10,13 +27,19 @@ int main(int argc, char **argv)
     }
 
     FILE *inp_file = fopen(argv[1], "r");
-    int x1,x2,y1,y2;
     fscanf(inp_file, "target area: x=%d..%d, y=%d..%d\n", &x1, &x2, &y1, &y2);
     fclose(inp_file);
 
-    int max_y = 0;
-    for (int i = 1;  i < abs(y1); ++i) {
-        max_y += i;
+    /* part 1 */
+    printf("%d\n", (y1*(y1+1))>>1);
+
+    /* part 2 */
+    int out = 0;
+    for (int vx=1; vx <= x2; ++vx) {
+        for (int vy=y1; vy < -y1; ++vy) {
+            out += launch(0, 0, vx, vy);
+        }
     }
-    printf("%d\n", max_y);
+
+    printf("%d\n", out);
 }
