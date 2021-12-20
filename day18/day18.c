d8eb315075de55eed2ffe8c886534a52142a8782
James Ho <ho.james142@gmail.com>
2021-12-20 22:14:13 +1100

    Day 18 pt 1
    I was stuck on this part as I misinterpreted the order in which to do
    the reduction. The data structure I used is not the best, as for every
    explode or split and even magnitude, I needed to shift the elements in
    the array, there is definitely a better structure for this, perhaps a
    linked list or a tree, and maybe recursion for the operations, but the
    array approach worked as the input is not too big and was relatively
    easy to implement in C.
    
    Lets hope I don't need to refactor the whole thing for part 2...

---
+++ day18/day18.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_CAP 1000
+
+typedef struct Pair {
+    int num;
+    int level;
+} PAIR;
+
+PAIR homework[BUFFER_CAP][BUFFER_CAP] = {0};
+int line_len[BUFFER_CAP] = {0};
+int n_lines = 0;
+PAIR buffer[BUFFER_CAP] = {0};
+int buffer_len = 0;
+
+void explode(int n)
+{
+    if (n == 0)
+        buffer[n+2].num += buffer[n+1].num;
+    else if (n >= buffer_len - 2)
+        buffer[n-1].num += buffer[n].num;
+    else {
+        buffer[n-1].num += buffer[n].num;
+        buffer[n+2].num += buffer[n+1].num;
+    }
+    buffer[n].num = 0;
+    --buffer[n].level;
+    if (n+1 < --buffer_len)
+        memmove(buffer+n+1, buffer+n+2, sizeof(PAIR) * (unsigned long)(buffer_len - n - 1));
+}
+
+void split_num(int n)
+{
+    memmove(buffer+n+2, buffer+n+1, sizeof(PAIR) * (unsigned long)(buffer_len++ - n - 1));
+    int prev = buffer[n].num;
+    buffer[n].num = prev >> 1;
+    ++buffer[n].level;
+    buffer[n+1].num = (prev+1) >> 1;
+    buffer[n+1].level = buffer[n].level;
+}
+
+void reduce_num(void)
+{
+    for (;;) {
+        int cont = 0;
+        for (int i=0; i<buffer_len; ++i) {
+            if (buffer[i].level >= 4) {
+                explode(i);
+                i = -1;
+            }
+        }
+        for (int i=0; i<buffer_len; ++i) {
+            if (buffer[i].num >= 10) {
+                split_num(i);
+                cont = 1;
+                break;
+            }
+        }
+        if (!cont)
+            break;
+    }
+}
+
+void addition(void)
+{
+    memcpy(buffer, homework[0], sizeof(PAIR) * (unsigned long)line_len[0]);
+    buffer_len = line_len[0];
+    for (int line=1; line<n_lines; ++line) {
+        memcpy(buffer+buffer_len, homework[line], sizeof(PAIR) * (unsigned long)line_len[line]);
+        buffer_len += line_len[line];
+        for (int i=0; i<buffer_len; ++i) {
+            ++buffer[i].level;
+        }
+        reduce_num();
+    }
+}
+
+int magnitude(void)
+{
+    for (int i=0; i<buffer_len-1; ++i) {
+        if (buffer[i].level == buffer[i+1].level) {
+            buffer[i].num = buffer[i].num * 3 + 2 * buffer[i+1].num;
+            --buffer[i].level;
+            if (i+1 < --buffer_len)
+                memmove(buffer+i+1, buffer+i+2, sizeof(PAIR) * (unsigned long)(buffer_len - i - 1));
+            i = -1;
+        }
+    }
+    return buffer[0].num;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        printf("Usage: %s <input file>\n", argv[0]);
+        exit(1);
+    }
+
+    FILE *fp = fopen(argv[1], "r");
+    int c = 0;
+    int l = -1;
+
+    while (c != EOF)
+    {
+        c = fgetc(fp);
+        if (c == ',')
+            continue;
+        else if (c == '[')
+            ++l;
+        else if (c == ']')
+            --l;
+        else if (c == '\n') {
+            ++n_lines;
+        } else {
+            homework[n_lines][line_len[n_lines]].num = c - '0';
+            homework[n_lines][line_len[n_lines]++].level = l;
+        }
+    }
+    fclose(fp);
+
+    addition();
+    /* for (int i=0; i<buffer_len; ++i) */
+    /*     printf("(%d, %d)", buffer[i].num, buffer[i].level); */
+    printf("%d\n", magnitude());
+}
